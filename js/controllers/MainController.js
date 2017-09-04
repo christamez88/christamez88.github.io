79e34645b3ff60aec00696df0906a76e2c69d196
christamez88 <tapthatsnare@yahoo.com>
2017-09-04 11:04:25 -0500

    update

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,18 +1,28 @@
 app.controller('MainController', ['$scope', function($scope) { 
-  $scope.title = 'Top Sellers in Books'; 
+  $scope.title = 'Best Sprint Workouts'; 
   $scope.products = 
 [ 
   { 
-    name: 'The Book of Trees', 
-    price: 19, 
-    pubdate: new Date('2014', '03', '08'), 
+    name: '16 x 100m', 
+    intensity: .85, 
+    timesPerWeek: 1, 
+    rest: '10-20 seconds',
     cover: 'https://www.google.com/google.jpg' 
   }, 
   { 
     name: 'Program or be Programmed', 
-    price: 8, 
-    pubdate: new Date('2013', '08', '01'), 
+    intensity: .85, 
+    timesPerWeek: 1, 
     cover: 'img/program-or-be-programmed.jpg' 
   } 
 ]
+    
+}]);
+
+  
+  // This filter makes the assumption that the input will be in decimal form (i.e. 17% is 0.17).
+app.filter('percentage', ['$filter', function ($filter) {
+  return function (input, decimals) {
+    return $filter('number')(input * 100, decimals) + '%';
+  };
 }]);
